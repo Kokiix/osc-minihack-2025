c3498e7e30955a8cdf4e9b829d462c4584fa8276
Kenneth <kokiidoki@gmail.com>
2025-09-27 14:58:56 -0400

    first attempt extracting data

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-console.log("hello world");
-
 let data = {};
 let courseIDs = {};
 axios.get("/api/v1/courses")
 .then(function (response) {
-    console.log(response.data);
-    for (i = 0; i < response.data.length; i++) {
+    for (i in response.data) {
         courseIDs[response.data[i].name] = response.data[i].id;
         data[response.data[i].name] = {};
     }
-
-    // for ()
+    console.log("Get course IDs and names");
+    
+    for (let courseName in courseIDs) {
+        axios.get(`/api/v1/courses/${courseIDs[courseName]}/modules?include[]=items`)
+            .then(function (response) {
+                let moduleData = response.data;
+                for (let module_i in moduleData) {
+                    data[courseName][moduleData[module_i].name] = []
+                    for (let assignment_i in moduleData[module_i].items) {
+                        data[courseName][moduleData[module_i].name][assignment_i] = moduleData[module_i].items[assignment_i]
+                    }
+                }
+        })
+    }
+    console.log("Get modules");
+    console.log(data)
 })
