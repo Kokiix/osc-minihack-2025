ca54c289a28afeff682ea7af4844d734f7bea245
Kenneth <kokiidoki@gmail.com>
2025-09-27 13:41:05 -0400

    running axios GET on random site

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("hello world")
+console.log("hello world");
+axios.get("https://httpbin.org/get")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  });
