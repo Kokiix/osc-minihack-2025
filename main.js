e514ec1db33c990d5cf825607b826d2fd7a364cb
Kenneth <kokiidoki@gmail.com>
2025-09-27 16:29:22 -0400

    first columns

--- main.js
+++ main.js
@@ -27,4 +27,45 @@ axios.get("/api/v1/courses")
     }
     console.log("Get modules");
     console.log(data)
+
+    // ken specific
+    delete data["Civic Literacy 2025"]
+    delete data["engi advising"]
+    delete data["mental health"]
+    delete data["undergrad research"]
+    delete data["Voices for change"]
+
+
+    document.head.innerHTML = `
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Dashboard</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+<style>
+  .columns .column:not(:last-child) {
+    border-right: 1px solid #dbdbdb;
+  }
+</style>`
+    document.body.innerHTML = `
+<div id="column-parent" class="columns has-text-centered"></div>`
+
+    for (let course in data) {
+        let column = `
+<div class="column">
+    <h2 class="title is-4 mt-4 has-text-centered">${course}</h2>
+    <hr class="is-divider">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+</div>`
+        document.getElementById("column-parent").insertAdjacentHTML("beforeend", column);
+    }
 })
+
+
+//   <div class="column">
+//     <h2 class="title is-4">Column 2 Header</h2>
+//     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+//   </div>
+//   <div class="column">
+//     <h2 class="title is-4">Column 3 Header</h2>
+//     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+//   </div>
